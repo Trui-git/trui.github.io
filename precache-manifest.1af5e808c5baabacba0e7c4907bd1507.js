bbbacbae204959e31111fe69b3654cf37089a555
Trui-git <34838847+Trui-git@users.noreply.github.com>
2020-04-30 10:45:06 -0400

    Add files via upload

---
+++ precache-manifest.1af5e808c5baabacba0e7c4907bd1507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1109d78a8c21b61d56b03ee6ddc84550",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b47e2f7ca1693f18092e",
+    "url": "/static/css/main.919285d5.chunk.css"
+  },
+  {
+    "revision": "afc206ece68bc0564db7",
+    "url": "/static/js/2.ba81a6e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba81a6e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47e2f7ca1693f18092e",
+    "url": "/static/js/main.0e602aae.chunk.js"
+  },
+  {
+    "revision": "38d1af05bb70db77cdf9",
+    "url": "/static/js/runtime-main.3093552a.js"
+  }
+]);
